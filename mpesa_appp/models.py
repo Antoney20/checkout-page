8088f5570da772f6023d057f7d6e602ece0d100c
ANTONY klerk <antomutwiri2001@gmail.com>
2023-07-05 16:05:58 +0300

    saving ata to database

--- mpesa_appp/models.py
+++ mpesa_appp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create your models here.\
+
+
 
 class Registration(models.Model):
+    
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     middle_name = models.CharField(max_length=255, blank=True)
@@ -12,6 +16,7 @@ class Registration(models.Model):
     email = models.EmailField()
     profile_image = models.ImageField(upload_to='profile_images/')
 
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
--- mpesa_appp/views.py
+++ mpesa_appp/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 import requests
 # Create your views here.
 from django.http import HttpResponse
 from .models import Registration
 def index(request):
     return HttpResponse('Welcome index')
+
+
+@login_required
 def checkout(request):
+    # Fetch the user's data from the database
+    user = request.user
+    # Access the user's fields
+    first_name = user.first_name
+    middle_name = user.middle_name
+    last_name = user.last_name
+    email = user.email
+    dob = user.dob
+    phone_number = user.phone_number
+    profile_image = user.profile_image
     return render(request, 'mpesa_appp/Checkout.html')
 
 def register(request):
@@ -13,12 +28,14 @@ def register(request):
         # Extract form data
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
-        middle_name = request.POST.get('middle_name', '')
+        middle_name = request.POST.get('middle_name')
         phone_number = request.POST.get('phone_number')
         password = request.POST.get('password')
         dob = request.POST.get('dob')
         email = request.POST.get('email')
         profile_image = request.FILES.get('profile_image')
+        
+        user = User.objects.create_user(username=first_name, password=password)
     
         registration = Registration(
             first_name=first_name,
@@ -31,7 +48,8 @@ def register(request):
             profile_image=profile_image,
         )
 
-        registration.save()
+        registration.save() 
+        return render(request, 'mpesa_appp/Checkout.html')
 
    
     else:
