5c642ccee51e3c0937f2bdefefb155ce4989f49c
ANTONY klerk <antomutwiri2001@gmail.com>
2023-07-06 16:06:39 +0300

    javascript files

---
+++ mpesa_appp/static/assets/js/idex.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $('.increase').click(function() {
+      var quantityElement = $(this).closest('.row').find('#quantity');
+      var priceElement = $(this).closest('tr').find('#price');
+      var itemTotalsElement = $(this).closest('tr').find('.item-totals');
+      
+      var quantity = parseInt(quantityElement.text());
+      var price = parseInt(priceElement.text());
+      
+      quantity++;
+      price = price * quantity;
+      
+      quantityElement.text(quantity);
+      priceElement.text(price);
+      itemTotalsElement.text(price);
+    });
+    
+    $('.decrease').click(function() {
+      var quantityElement = $(this).closest('.row').find('#quantity');
+      var priceElement = $(this).closest('tr').find('#price');
+      var itemTotalsElement = $(this).closest('tr').find('.item-totals');
+      
+      var quantity = parseInt(quantityElement.text());
+      var price = parseInt(priceElement.text());
+      
+      if (quantity > 0) {
+        quantity--;
+        price = price * quantity;
+        
+        quantityElement.text(quantity);
+        priceElement.text(price);
+        itemTotalsElement.text(price);
+      }
+      
+      if (quantity === 0) {
+        
+        $(this).closest('tr').remove();
+      }
+    });
+    
+
+    $('.remove').click(function() {
+      $(this).closest('tr').remove();
+    });
+  });
+  
